c8c528ae42d87aac8ac83beb0211c4471d05e0ee
lisa <3114618002@qq.com>
2022-02-26 17:14:27 +0800

    'last'

--- js/login.js
+++ js/login.js
@@ -7,27 +7,5 @@ clo.onclick = function(){
 gin.onclick = function(){
     box_.style.display='block'
 }
-<<<<<<< HEAD
 
-count1.onblur=function(){
-    var reg=/[a-zA-Z0-9]{1,10}@[a-zA-Z0-9]{1,5}\.[a-zA-Z0-9]{1,5}|^1[3578]\d{9}$|^[\u4e00-\u9fa5]{2,4}$/;
-    if(reg.test(count1.value)){
-        tishi1.innerHtml='输入正确';
-        tishi1.style.color='green';
-    }else{
-        tishi1.innerHtml='输入错误';
-        tishi1.style.color='red';
-    }
-}
-sec1.onblur=function(){
-    var reg=/^\w{6,10}$/;
-    if(reg.test(sec1.value)){
-        tishi2.innerHtml='输入正确';
-        tishi2.style.color='green';
-    }else{
-        tishi2.innerHtml='输入错误';
-        tishi2.style.color='red';
-    }
-}
-=======
->>>>>>> a99bae11f90278bb7e232adbc557b227297ab032
+
